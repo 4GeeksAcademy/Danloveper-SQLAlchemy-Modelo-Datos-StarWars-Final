30fdb859e23ef590b4e6d301c9cd52925974fa55
Danloveper <danloveper@gmail.com>
2025-09-04 03:32:55 +0000

    Add/ Create models to intagram and relations

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, Integer, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
@@ -10,6 +10,7 @@ class User(db.Model):
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+    favorities: Mapped[list['Favorite']] = relationship(back_populates="user")
 
     def serialize(self):
         return {
@@ -17,3 +18,49 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+class Character(db.Model):
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    model_name: Mapped[str] = mapped_column(Integer)
+    name: Mapped[str] = mapped_column(String, unique=True)
+    gender: Mapped[str] = mapped_column(String)
+    hair_color: Mapped[str] = mapped_column(String)
+    eye_color: Mapped[str] = mapped_column(String)
+
+class Planet(db.Model):
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    model_name: Mapped[str] = mapped_column(Integer)
+    name: Mapped[str] = mapped_column(String, unique=True)
+    climate: Mapped[str] = mapped_column(String)
+    population: Mapped[int] = mapped_column(Integer)
+    orbital_period: Mapped[int] = mapped_column(Integer)
+    rotation_period: Mapped[int] = mapped_column(Integer)
+    diameter: Mapped[int] = mapped_column(Integer)
+
+class Vehicle(db.Model):
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    model_name: Mapped[str] = mapped_column(Integer)
+    name: Mapped[str] = mapped_column(String, unique=True)
+    cargo_capacity: Mapped[int] = mapped_column(Integer)
+    consumables: Mapped[str] = mapped_column(String)
+    cost_in_credits: Mapped[int] = mapped_column(Integer)
+
+class Favorite(db.Model):
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+    model: Mapped[str] = mapped_column(String(50))
+    model_id: Mapped[int] = mapped_column(Integer)
+
+    user: Mapped['User'] = relationship(back_populates="favorities")
+
+    def get_item(self):
+        if self.model == "character":
+            return db.session.get(Character, self.model_id)
+        elif self.model == "planet":
+            return db.session.get(Planet, self.model_id)
+        elif self.model == "vehicle":
+            return db.session.get(Planet, self.model_id)
